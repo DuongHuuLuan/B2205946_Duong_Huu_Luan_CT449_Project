d61e67d9d0242968b75238cd894b6387a70113f7
DuongHuuLuan <luanb2205946@student.ctu.edu.vn>
2025-10-07 00:18:52 +0700

    Cap nhat Dang ky Dang nhap cho Doc Gia

---
+++ frontend/apps/QLMS-frontend-user/src/views/auth/Login.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-lg-4">
+                <!-- Gọi component LoginForm -->
+                <LoginForm />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import LoginForm from "@/components/auth/LoginForm.vue";
+
+export default {
+    name: "login",
+    components: { LoginForm },
+};
+</script>
